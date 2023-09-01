e159241d186594f389e97fc8fd9bb91aec0806c5
Yasmin Sobrinho <yasmin.sobrinho@unesp.br>
2023-09-01 00:22:41 -0300

    first commit
    first piece of code and gitignore setup

---
+++ clinic.c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef struct {
+    int clientCode;
+    char clientName[20];
+    int clientAge;
+    char clientAddress[40];
+    char clientExist;
+} typeClient;
+
+void addClient(FILE * clientData) {
+    typeClient clientList;
+}
