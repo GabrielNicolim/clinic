7f6923e25333879c41c43b93d817e6204e9a12fa
Yasmin Sobrinho <yasmin.sobrinho@unesp.br>
2023-09-12 11:15:11 -0300

    Function: updateClient
    This function updates previously added client data in the file (clientData)

--- clinic.c.c
+++ clinic.c.c
@@ -41,9 +41,10 @@ void addClient(FILE *clientData) {
 
         printf("Endereco: ");
         gets(client.clientAddress.street);
+        scanf("%d", &client.clientAddress.houseNumber);
         gets(client.clientAddress.city);
         gets(client.clientAddress.state);
-        scanf("%d", &client.clientAddress.houseNumber);
+
 
         fwrite(&client, sizeof(typeClient), 1, clientData);
 
@@ -55,3 +56,63 @@ void addClient(FILE *clientData) {
 
     fclose(clientData);
 }
+
+void updateClient(FILE *clientData) {
+    typeClient client;
+    char searchName[30], answer;
+    int clientFlag = 0;
+
+    FILE *clientData
+
+    if (!clientData = fopen("clientFile.dat", "rb")) {
+        printf("Houve um erro na abertura do arquivo.");
+        return 1;
+    }
+
+    printf("\nCONSULTA DE PACIENTES PARA ATUALIZACAO\n");
+    printf("Digite o nome do cliente a ser buscado: ");
+    scanf("%s", searchName);
+
+    while (fread(&client, sizeof(typeClient), 1, clientData)) {
+        if (client.clientExists && strcmp(client.clientName, searchName) == 0) {
+
+            printf("\nDADOS ATUAIS DO PACIENTE BUSCADO\n");
+
+            printf("Codigo: %d\n", client.clientCode);
+            printf("Nome: %s\n", client.clientName);
+            printf("Idade: %d", client.clientAge);
+            printf("Endereco: %s, %d (%s-%s)", client.clientAddress.street, client.clientAddress.houseNumber, client.clientAddress.city, client.clientAddress.state);
+            
+            printf("\nATUALIZACAO DO CADASTRO DO PACIENTE\n");
+
+            printf("Codigo: ");
+            scanf("%d", &client.clientCode);
+            fflush(stdin);
+
+            printf("Nome: ");
+            gets(client.clientName);
+
+            printf("Idade: ");
+            scanf("%d", &client.clientAge);
+            fflush(stdin);
+
+            printf("Endereco: ");
+            gets(client.clientAddress.street);
+            scanf("%d", &client.clientAddress.houseNumber);
+            gets(client.clientAddress.city);
+            gets(client.clientAddress.state);
+
+
+            fseek(clientData, -sizeof(typeClient), SEEK_CUR);
+            fwrite(&client, sizeof(typeClient), 1, clientData);
+            clientFlag = 1;
+            break;
+        }
+    }
+
+    if (!clientFlag) {
+        printf("\nO paciente buscado nao foi encontrado no banco de dados.\n");
+    }
+    fclose(productsData);
+}
+
