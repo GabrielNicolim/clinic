9c069c0f45c110fd1873ca1147ab8d0fdfdfda78
Yasmin Sobrinho <yasmin.sobrinho@unesp.br>
2023-09-12 23:28:52 -0300

    Subtle changes

--- clinic.c
+++ clinic.c
@@ -2,8 +2,10 @@
 #include <ctype.h>
 
 #ifdef _WIN32
+
 #include <conio.h>
 #include <windows.h>
+
 #endif
 
 typedef struct {
@@ -23,7 +25,7 @@ void addClient() {
     typeClient client;
     char answer;
 
-	FILE *clientData;
+    FILE *clientData;
 
     if (!(clientData = fopen("clientFile.dat", "a+b"))) {
         printf("Houve um erro na abertura do arquivo.\n");
@@ -45,10 +47,17 @@ void addClient() {
         scanf("%d", &client.clientAge);
         fflush(stdin);
 
-        printf("Endereco: ");
+        printf("Rua: ");
         gets(client.clientAddress.street);
+
+        printf("Numero da casa: ");
         scanf("%d", &client.clientAddress.houseNumber);
+        fflush(stdin);
+
+        printf("Cidade: ");
         gets(client.clientAddress.city);
+
+        printf("Estado: ");
         gets(client.clientAddress.state);
 
 
@@ -68,101 +77,107 @@ void updateClient() {
     char searchName[30], answer;
     int clientFlag = 0;
 
-	FILE *clientData;
+    FILE *clientData;
 
-    if (! (clientData = fopen("clientFile.dat", "rb"))) {
+    if (!(clientData = fopen("clientFile.dat", "a+b"))) {
         printf("Houve um erro na abertura do arquivo.");
 
-		return;
-	}
+        return;
+    }
+    do {
 
-    printf("\nCONSULTA DE PACIENTES PARA ATUALIZACAO\n");
-    printf("Digite o nome do cliente a ser buscado: ");
-    scanf("%s", searchName);
+        printf("\nCONSULTA DE PACIENTES PARA ATUALIZACAO\n");
+        printf("Digite o nome do cliente a ser buscado: ");
+        scanf("%s", searchName);
 
-    while (fread(&client, sizeof(typeClient), 1, clientData)) {
-        if (client.clientExists && strcmp(client.clientName, searchName) == 0) {
+        while (fread(&client, sizeof(typeClient), 1, clientData)) {
+            if (client.clientExists && strcmp(client.clientName, searchName) == 0) {
 
-            printf("\nDADOS ATUAIS DO PACIENTE BUSCADO\n");
+                printf("\nDADOS ATUAIS DO PACIENTE BUSCADO\n");
 
-            printf("Codigo: %d\n", client.clientCode);
-            printf("Nome: %s\n", client.clientName);
-            printf("Idade: %d", client.clientAge);
-            printf("Endereco: %s, %d (%s-%s)", client.clientAddress.street, client.clientAddress.houseNumber, client.clientAddress.city, client.clientAddress.state);
-            
-            printf("\nATUALIZACAO DO CADASTRO DO PACIENTE\n");
+                printf("Codigo: %d\n", client.clientCode);
+                printf("Nome: %s\n", client.clientName);
+                printf("Idade: %d", client.clientAge);
+                printf("Endereco: %s, %d (%s-%s)", client.clientAddress.street, client.clientAddress.houseNumber,
+                       client.clientAddress.city, client.clientAddress.state);
 
-            printf("Codigo: ");
-            scanf("%d", &client.clientCode);
-            fflush(stdin);
+                printf("\nATUALIZACAO DO CADASTRO DO PACIENTE\n");
 
-            printf("Nome: ");
-            gets(client.clientName);
+                printf("Codigo: ");
+                scanf("%d", &client.clientCode);
+                fflush(stdin);
 
-            printf("Idade: ");
-            scanf("%d", &client.clientAge);
-            fflush(stdin);
+                printf("Nome: ");
+                gets(client.clientName);
 
-            printf("Endereco: ");
-            gets(client.clientAddress.street);
-            scanf("%d", &client.clientAddress.houseNumber);
-            gets(client.clientAddress.city);
-            gets(client.clientAddress.state);
+                printf("Idade: ");
+                scanf("%d", &client.clientAge);
+                fflush(stdin);
+
+                printf("Endereco: ");
+                gets(client.clientAddress.street);
+                scanf("%d", &client.clientAddress.houseNumber);
+                gets(client.clientAddress.city);
+                gets(client.clientAddress.state);
 
 
-            fseek(clientData, -sizeof(typeClient), SEEK_CUR);
-            fwrite(&client, sizeof(typeClient), 1, clientData);
-            clientFlag = 1;
+                fseek(clientData, -sizeof(typeClient), SEEK_CUR);
+                fwrite(&client, sizeof(typeClient), 1, clientData);
+                clientFlag = 1;
 
-			break;
+                break;
+            }
         }
-    }
 
-    if (!clientFlag) {
-        printf("\nO paciente buscado nao foi encontrado no banco de dados.\n");
-    }
+        if (!clientFlag) {
+            printf("\nO paciente buscado nao foi encontrado no banco de dados.\n");
+        }
+
+        printf("\n Deseja adicionar outro cliente (S/N)? ");
+        do {
+            answer = toupper(getch());
+        } while (answer != 'S' && answer != 'N');
+    } while (answer == 'S');
 }
 
-int main()
-{
-	int option = 0;
+int main() {
+    int option = 0;
 
-	do {
-		printf("1 - Cadastrar cliente\n");
-		printf("2 - Alterar dados de cliente\n");
-		printf("3 - Marcar consulta\n");
-		printf("4 - Desmarcar a consulta\n");
-		printf("5 - Escrever mapa de horários para um determinado dia\n");
-		printf("6 - Obter consultas\n");
-		printf("7 - Consultar maiores de 50\n");
-		printf("8 - Fim\n\n");
-		
-		printf("Escolhar uma opcao (1 - 8): ");
-		scanf("%d", &option);
+    do {
+        printf("1 - Cadastrar cliente\n");
+        printf("2 - Alterar dados de cliente\n");
+        printf("3 - Marcar consulta\n");
+        printf("4 - Desmarcar a consulta\n");
+        printf("5 - Escrever mapa de horï¿½rios para um determinado dia\n");
+        printf("6 - Obter consultas\n");
+        printf("7 - Consultar maiores de 50\n");
+        printf("8 - Fim\n\n");
 
-		system("cls");
-	} while(option < 1 || option > 8);
-	
-	switch(option)
-	{
-		case 1:
-			addClient();
-			break;
-		case 2:
-			updateClient();
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		case 7:
-			break;
-		case 8:
-			break;
-	}
+        printf("Escolhar uma opcao (1 - 8): ");
+        scanf("%d", &option);
+
+        system("cls");
+    } while (option < 1 || option > 8);
+
+    switch (option) {
+        case 1:
+            addClient();
+            break;
+        case 2:
+            updateClient();
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+    }
 }
 
