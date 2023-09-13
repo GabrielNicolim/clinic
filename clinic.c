22251aceb8e1b665616176eb76f0f314922f257c
Yasmin Sobrinho <yasmin.sobrinho@unesp.br>
2023-09-12 23:53:40 -0300

    Update updateClient
    Search by: codeClient

--- clinic.c
+++ clinic.c
@@ -18,6 +18,7 @@ typedef struct {
     char clientName[20];
     int clientAge;
     typeAddress clientAddress;
+    int clientPhoneNumber;
     char clientExists;
 } typeClient;
 
@@ -47,6 +48,10 @@ void addClient() {
         scanf("%d", &client.clientAge);
         fflush(stdin);
 
+        printf("Telefone: ");
+        scanf("%d", &client.clientPhoneNumber);
+        fflush(stdin);
+
         printf("Rua: ");
         gets(client.clientAddress.street);
 
@@ -60,10 +65,9 @@ void addClient() {
         printf("Estado: ");
         gets(client.clientAddress.state);
 
-
         fwrite(&client, sizeof(typeClient), 1, clientData);
 
-        printf("\n Deseja adicionar outro cliente (S/N)? ");
+        printf("\nDeseja adicionar outro paciente (S/N)? ");
         do {
             answer = toupper(getch());
         } while (answer != 'S' && answer != 'N');
@@ -74,8 +78,8 @@ void addClient() {
 
 void updateClient() {
     typeClient client;
-    char searchName[30], answer;
-    int clientFlag = 0;
+    char answer;
+    int clientFlag = 0, searchCode;
 
     FILE *clientData;
 
@@ -87,19 +91,26 @@ void updateClient() {
     do {
 
         printf("\nCONSULTA DE PACIENTES PARA ATUALIZACAO\n");
-        printf("Digite o nome do cliente a ser buscado: ");
-        scanf("%s", searchName);
+        printf("Digite o codigo do paciente a ser buscado: ");
+        scanf("%d", &searchCode);
 
         while (fread(&client, sizeof(typeClient), 1, clientData)) {
-            if (client.clientExists && strcmp(client.clientName, searchName) == 0) {
+            if (client.clientExists && searchCode) {
 
                 printf("\nDADOS ATUAIS DO PACIENTE BUSCADO\n");
 
                 printf("Codigo: %d\n", client.clientCode);
                 printf("Nome: %s\n", client.clientName);
-                printf("Idade: %d", client.clientAge);
-                printf("Endereco: %s, %d (%s-%s)", client.clientAddress.street, client.clientAddress.houseNumber,
-                       client.clientAddress.city, client.clientAddress.state);
+                printf("Idade: %d\n", client.clientAge);
+                printf("Telefone: %d\n", client.clientPhoneNumber);
+
+                printf("Endereco:\n");
+
+                printf("Rua: %s", client.clientAddress.street);
+                printf("Numero da casa: %d", client.clientAddress.houseNumber);
+                printf("Cidade: %s", client.clientAddress.city);
+
+                printf("Estado: %s", client.clientAddress.state);
 
                 printf("\nATUALIZACAO DO CADASTRO DO PACIENTE\n");
 
@@ -114,17 +125,28 @@ void updateClient() {
                 scanf("%d", &client.clientAge);
                 fflush(stdin);
 
-                printf("Endereco: ");
-                gets(client.clientAddress.street);
-                scanf("%d", &client.clientAddress.houseNumber);
-                gets(client.clientAddress.city);
-                gets(client.clientAddress.state);
+                printf("Telefone: ");
+                scanf("%d", &client.clientPhoneNumber);
+                fflush(stdin);
 
+                printf("Endereco:\n\n");
+
+                printf("Rua: ");
+                gets(client.clientAddress.street);
+
+                printf("Numero da casa: ");
+                scanf("%d", &client.clientAddress.houseNumber);
+                fflush(stdin);
+
+                printf("Cidade: ");
+                gets(client.clientAddress.city);
+
+                printf("Estado: ");
+                gets(client.clientAddress.state);
 
                 fseek(clientData, -sizeof(typeClient), SEEK_CUR);
                 fwrite(&client, sizeof(typeClient), 1, clientData);
                 clientFlag = 1;
-
                 break;
             }
         }
@@ -133,7 +155,7 @@ void updateClient() {
             printf("\nO paciente buscado nao foi encontrado no banco de dados.\n");
         }
 
-        printf("\n Deseja adicionar outro cliente (S/N)? ");
+        printf("\n Deseja adicionar outro paciente (S/N)? ");
         do {
             answer = toupper(getch());
         } while (answer != 'S' && answer != 'N');
@@ -144,11 +166,11 @@ int main() {
     int option = 0;
 
     do {
-        printf("1 - Cadastrar cliente\n");
-        printf("2 - Alterar dados de cliente\n");
+        printf("1 - Cadastrar paciente\n");
+        printf("2 - Alterar dados de paciente\n");
         printf("3 - Marcar consulta\n");
         printf("4 - Desmarcar a consulta\n");
-        printf("5 - Escrever mapa de hor�rios para um determinado dia\n");
+        printf("5 - Escrever mapa de horarios para um determinado dia\n");
         printf("6 - Obter consultas\n");
         printf("7 - Consultar maiores de 50\n");
         printf("8 - Fim\n\n");
